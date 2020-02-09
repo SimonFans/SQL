e33d4163b8180a2dc366f3175b3f1ae1543591c1
Simon Zhao <Ximengzhao1220@gmail.com>
2020-02-09 09:19:00 -0800

    Create L1083_Sales_Analysis.sql

---
+++ L1083_Sales_Analysis.sql
@@ -0,0 +1,85 @@
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
+| unit_price   | int     |
++--------------+---------+
+product_id is the primary key of this table.
+Table: Sales
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| seller_id   | int     |
+| product_id  | int     |
+| buyer_id    | int     |
+| sale_date   | date    |
+| quantity    | int     |
+| price       | int     |
++------ ------+---------+
+This table has no primary key, it can have repeated rows.
+product_id is a foreign key to Product table.
+ 
+
+Write an SQL query that reports the buyers who have bought S8 but not iPhone. Note that S8 and iPhone are products present in the Product table.
+
+The query result format is in the following example:
+
+Product table:
++------------+--------------+------------+
+| product_id | product_name | unit_price |
++------------+--------------+------------+
+| 1          | S8           | 1000       |
+| 2          | G4           | 800        |
+| 3          | iPhone       | 1400       |
++------------+--------------+------------+
+
+Sales table:
++-----------+------------+----------+------------+----------+-------+
+| seller_id | product_id | buyer_id | sale_date  | quantity | price |
++-----------+------------+----------+------------+----------+-------+
+| 1         | 1          | 1        | 2019-01-21 | 2        | 2000  |
+| 1         | 2          | 2        | 2019-02-17 | 1        | 800   |
+| 2         | 1          | 3        | 2019-06-02 | 1        | 800   |
+| 3         | 3          | 3        | 2019-05-13 | 2        | 2800  |
++-----------+------------+----------+------------+----------+-------+
+
+Result table:
++-------------+
+| buyer_id    |
++-------------+
+| 1           |
++-------------+
+The buyer with id 1 bought an S8 but didn't buy an iPhone. The buyer with id 3 bought both.
+
+
+# solution1:
+
+select 
+    distinct s.buyer_id
+from Sales s
+join Product p
+on s.product_id=p.product_id
+where p.product_name='S8' 
+and s.buyer_id not in (
+    select 
+        s1.buyer_id
+    from Sales s1
+    join Product p1
+    on s1.product_id=p1.product_id
+    where p1.product_name='iPhone'
+)
+
+# solution2:
+
+SELECT s.buyer_id
+FROM Sales AS s INNER JOIN Product AS p
+ON s.product_id = p.product_id
+GROUP BY s.buyer_id
+HAVING SUM(CASE WHEN p.product_name = 'S8' THEN 1 ELSE 0 END) > 0
+AND SUM(CASE WHEN p.product_name = 'iPhone' THEN 1 ELSE 0 END) = 0;
+
+
