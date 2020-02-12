7be7d825d9bd140ca335a37c96a9fc45d6a82dd1
Simon Zhao <Ximengzhao1220@gmail.com>
2020-02-11 23:32:55 -0800

    Update L597_Friend Requests I: Overall Acceptance Rate.sql

--- L597_Friend Requests I: Overall Acceptance Rate.sql
+++ L597_Friend Requests I: Overall Acceptance Rate.sql
@@ -24,6 +24,18 @@ For the sample data above, your query should return the following result.
 |       0.80|
 --------------------- 
 
+
+-- solution 1:
+
+select 
+round(
+    ifnull(
+    (select count(*) from (select distinct requester_id, accepter_id from request_accepted) a) 
+/ 
+    (select count(*) from (select distinct sender_id, send_to_id from friend_request) b ),0),2) as accept_rate
+
+-- solution 2:
+
 select coalesce(round
 (count(distinct requester_id, accepter_id)
 /
